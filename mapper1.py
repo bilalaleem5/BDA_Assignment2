6787966740528cac98f53f307a0330fb78986a20
bilalaleem5 <154947845+bilalaleem5@users.noreply.github.com>
2024-03-31 22:14:47 +0500

    Update mapper1.py

--- mapper1.py
+++ mapper1.py
@@ -9,6 +9,7 @@ def mapper():
         if len(columns) < 4:
             continue
         words = columns[3].lower().translate(str.maketrans('', '', string.punctuation)).split()
+        
         for word in words:
             print(f"{word}\t1")
 
